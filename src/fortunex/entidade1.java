d26efb1cb4c95400a67a822da281a8cae089bafa
primeiro <karolsouza12345k1234k@gmail.com>
2025-09-19 23:01:50 -0300

    comentarios //

--- src/fortunex/entidade1.java
+++ src/fortunex/entidade1.java
@@ -1,4 +1,6 @@
-package fortunex.entidades;
-
-public class entidade1 {
-}
+package fortunex;
+//aqui vai ser uma das subclasses
+public class entidade1 extends rename {
+}// tem q sempre colocar essa escrita "extends rename" para indicar que essa classe é uma subclasse de rename
+//troquem o nome da classe entidade1 para o nome que quiserem, mas sempre lembrando de colocar "extends rename"
+//façam o mesmo para as outras classes entidade2, entidade3 e entidade4
--- src/fortunex/rename.java
+++ src/fortunex/rename.java
@@ -1,4 +1,6 @@
 package fortunex;
-
+//aqui vai ser a classe abstrata que vai ser extendida pelas outras classes
+//ela vai ter os atributos e metodos comuns a todas as classes
+//e tambem vai ter os metodos abstratos que vao ser implementados nas subclasses
 public class rename {
 }
