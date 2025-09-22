0e2afbf65b6a749f4363f4a7c4a1378c9be5256c
JoaoHenrique2109 <jhenriqueb2109@gmail.com>
2025-09-22 17:13:53 -0300

    teste

--- src/fortunex/classes/Usuario.java
+++ src/fortunex/classes/Usuario.java
@@ -1,7 +1,16 @@
 package fortunex.classes;
-
-//aqui vai ser uma das subclasses
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 public class Usuario extends rename {
-}// tem q sempre colocar essa escrita "extends rename" para indicar que essa classe é uma subclasse de rename
+
+    String nome = "joao";
+}
+
+
+
+// tem q sempre colocar essa escrita "extends rename" para indicar que essa classe é uma subclasse de rename
 //troquem o nome da classe entidade1 para o nome que quiserem, mas sempre lembrando de colocar "extends rename"
 //façam o mesmo para as outras classes entidade2, entidade3 e entidade4
