80d387dd011f98cbbae8bd10b4826a4d1a659ff0
primeiro <karolsouza12345k1234k@gmail.com>
2025-09-23 20:09:35 -0300

    coloquei uma coisa na classe usuario

--- src/fortunex/classes/Usuario.java
+++ src/fortunex/classes/Usuario.java
@@ -11,6 +11,6 @@ public class Usuario extends rename {
 
 
 
-// tem q sempre colocar essa escrita "extends rename" para indicar que essa classe é uma subclasse de rename
+// tem que sempre colocar essa escrita "extends rename" para indicar que essa classe é uma subclasse de rename
 //troquem o nome da classe entidade1 para o nome que quiserem, mas sempre lembrando de colocar "extends rename"
 //façam o mesmo para as outras classes entidade2, entidade3 e entidade4
